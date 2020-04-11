156a44ca9eb814d127fd574540ed4166a37d1eb2
Skantz <danska@kth.se>
2020-04-11 18:56:04 +0900

    good

--- mlp/mnist.py
+++ mlp/mnist.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as pyplot
 
 @dataclass
 class BatchStatistics:
+    #todo. keep tensors or convert all to list at first
     batch_size : int
     loss_sum   : float
     accu_list  : list
@@ -18,35 +19,71 @@ class BatchStatistics:
 @dataclass
 class Evaluation:
     categories_n     : int
+    num_epochs       : int
     batch_statistics : list
-    loss_by_category_over_batches : list
 
-    def plot_loss(self):
-        losses = [batch.loss_sum for batch in self.batch_statistics]
+    def plot_loss(self, fig_path):
+        losses = [batch.loss_sum.item() for batch in self.batch_statistics]
+        losses = [sum([bloss for bloss in epoch])/len(epoch)
+                 for epoch in [losses[i: i + len(losses)//self.num_epochs] for i in range(0, len(losses), len(losses)//self.num_epochs)]]
+
         pyplot.plot(losses)
         pyplot.show()
+        pyplot.savefig(fig_path)
+        pyplot.clf()
+
+    def plot_accu(self, fig_path):
+        accus = [batch.accu_mean for batch in self.batch_statistics]
+        accus = [sum([baccu for baccu in epoch])/len(epoch)
+                 for epoch in [accus[i: i + len(accus)//self.num_epochs] for i in range(0, len(accus), len(accus)//self.num_epochs)]]
+        pyplot.plot(accus)
+        pyplot.show()
+        pyplot.savefig(fig_path)
+        pyplot.clf()
+
+    def print_summary(self):
+        #redundant .item. 
+        loss_epochs = [batch.loss_sum.item()for batch in self.batch_statistics]
+        loss_total = sum(loss_epochs) / len(loss_epochs)
+        accu_epochs = [batch.accu_mean for batch in self.batch_statistics]
+        accu_total = sum(accu_epochs) / len(loss_epochs)
+        print("loss:", loss_total)
+        print("accu:", accu_total)
 
     def calc_loss_by_category(self):
         correct_by_category   = [0 for _ in range(self.categories_n)]
         samples_n_by_category = [0 for _ in range(self.categories_n)]
         for bs in self.batch_statistics:
             for i, (pred, label) in enumerate(zip(bs.all_preds, bs.all_labels)):
-                samples_n_by_category[label[i]] += 1
-                if torch.argmax(pred[i], dim=1) == label[i]:
-                    correct_by_category[label[i]] += 1 
-        print(correct_by_category)
+                samples_n_by_category[label.item()] += 1
+                if torch.argmax(pred, dim=-1).item() == label.item():
+                    correct_by_category[label.item()] += 1 
 
+        return [score/all for (score, all) in zip(correct_by_category, samples_n_by_category)]
 
-    def plot_loss_by_category(self):
-        pass
+    def plot_loss_by_category(self, fig_path, loss_by_category=None):
+
+        if not loss_by_category:
+            loss_by_category = self.calc_loss_by_category()
+
+        pyplot.clf()
+        pyplot.bar(range(0, len(loss_by_category)), loss_by_category, color='blue')
+        pyplot.show()
+        pyplot.savefig(fig_path)      
 
 
 class MLP(torch.nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
+        self.linear = torch.nn.Linear(28 * 28, 10)
     
-    def forward(self, x):
-        pass
+    def forward(self, x, label):
+        #view ?
+        x = x.reshape(-1, 28* 28)
+        x = self.linear(x)
+        label = torch.nn.functional.one_hot(label, 10).float()
+        loss  = torch.nn.functional.binary_cross_entropy_with_logits(x, label)
+        return loss.mean(dim=0), x
 
 
 class ConvNet(torch.nn.Module):
@@ -65,12 +102,14 @@ class ConvNet(torch.nn.Module):
         x = self.fcl2(x)
         label = torch.nn.functional.one_hot(label, 10).float()
         loss  = torch.nn.functional.binary_cross_entropy_with_logits(x, label)
-        return loss, x
+        return loss.mean(dim=0), x
 
-def train():
 
-    model = ConvNet()
-    batch_size = 32
+def train(device):
+
+    #model = ConvNet()
+    model = MLP()
+    batch_size = 64
     dataloader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST('./', train=True, download=True,
                                 transform=torchvision.transforms.Compose([
@@ -79,52 +118,77 @@ def train():
                                         (0.1307,), (0.3081,))])),
         batch_size=batch_size, shuffle=True)
 
-    lr = 0.01
-    epochs = 10
-    optimizer = torch.optim.Adam(model.parameters(), lr, )
-    display_inverse_freq = 500
+    lr = 0.001 
+    epochs = 50
+    optimizer = torch.optim.SGD(model.parameters(), lr, )
+    display_inverse_freq = len(dataloader) + 1
+
+    model.to(device)
+    for p in model.parameters():
+        p.to(device)
 
     all_batch_stats = []
     for e in range(epochs):
         batch_iter = 0
         for img, label in dataloader:
+            img = img.to(device)
+            label = label.to(device)
             optimizer.zero_grad()
             loss, p = model.forward(img, label)
             loss.backward()
             optimizer.step()
-            #couöd use a normal class
+            #could use a normal class
             bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == label,
                                  (torch.argmax(p, dim=1) == label).sum().item()/batch_size,
                                  p, label, epoch=e)
             all_batch_stats.append(bs)
-    
-            if batch_iter % display_inverse_freq:
+
+            if batch_iter % display_inverse_freq == 0:
                 print("batch loss", bs.loss_sum.item())
                 print("batch acc", bs.accu_mean, "%")
+                print("progress", (e/epochs) * 100 + batch_iter / len(dataloader) / epochs * 100, "%")
             batch_iter +=1
 
-    return model, stats
+    return model, all_batch_stats
 
-def test(model):
+def test(model, device):
 
+    batch_size = 64
     dataloader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST('./', train=False, download=True,
                                 transform=torchvision.transforms.Compose([
                                     torchvision.transforms.ToTensor(),
                                     torchvision.transforms.Normalize(
                                         (0.1307,), (0.3081,))])),
-        batch_size=32, shuffle=True)
+        batch_size=batch_size, shuffle=True)
 
-    stats = Evaluation()
+    all_batch_stats = []
     for img, label in dataloader:
-        with torch.no_grad:
+        img = img.to(device)
+        label = label.to(device)
+        with torch.no_grad():
             loss, p = model.forward(img, label)
+        bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == label,
+                                 (torch.argmax(p, dim=1) == label).sum().item()/batch_size,
+                                 p, label, epoch=0)
+        all_batch_stats.append(bs)
 
-
+    return all_batch_stats
 
 def main():
-    model, stats = train()
-    test(model)
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    model, stats = train(device)
+    analysis = Evaluation(10, 10, stats)
+    analysis.plot_loss("train_loss.png")
+    analysis.plot_accu("train_accu.png")
+    analysis.calc_loss_by_category()
+    stats = test(model, device)
+    analysis = Evaluation(10, 1, stats)
+    analysis.calc_loss_by_category()
+    analysis.plot_loss_by_category("test.png")
+    analysis.print_summary()
 
 if __name__ == "__main__":
     main()
