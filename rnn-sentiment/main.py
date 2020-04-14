59556cada5c25567fc9c3850d1a4d8418ed7a6db
Skantz <danska@kth.se>
2020-04-15 03:45:13 +0900

    43%, no optimization

--- rnn-sentiment/main.py
+++ rnn-sentiment/main.py
@@ -6,7 +6,7 @@ from model import SentRNN
 
 def train(model, device, loader, batch_size, lr, optimizer):
 
-    epochs = 30
+    epochs = 2
     if optimizer == "SGD":
         optimizer = torch.optim.SGD(model.parameters(), lr, )
     else:
@@ -18,53 +18,70 @@ def train(model, device, loader, batch_size, lr, optimizer):
         p.to(device)
 
     all_batch_stats = []
-    loader = iter(loader)
-    img = next(loader)
+
+    grad_clip_limit = 5
+
+    #loader = torch.nn.utils.rnn.pad_sequence(loader)
+    #loader = torch.nn.utils.rnn.pack_padded_sequence(loader)
     for e in range(epochs):
         batch_iter = 0
-        while img:
-
+        for img in loader:
             label = img.label
             img = img.text
 
             label = label - 1
             label = torch.nn.functional.one_hot(label, num_classes=5) 
-            label = label.unsqueeze(0)
+            label = label.unsqueeze(1)
 
             img = img.to(device)
             label = label.to(device)
             optimizer.zero_grad()
             loss, p = model.forward(img, label)
+
             loss.backward()
             optimizer.step()
             #could use a normal class
-            bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == label,
-                                 (torch.argmax(p, dim=1) == label).sum().item()/batch_size,
-                                 p, label, epoch=e)
+            #print(p.shape)
+            #print(label.shape)
+            label = label.squeeze(1)
+            bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == torch.argmax(label, dim=1),
+                                 (torch.argmax(p, dim=1) == torch.argmax(label, dim=1)).sum().item()/batch_size,
+                                 p, torch.argmax(label, dim=1), epoch=e)
             all_batch_stats.append(bs)
 
             if batch_iter % display_inverse_freq == 0:
                 print("batch loss", bs.loss_sum.item())
-                print("batch acc", bs.accu_mean, "%")
+                print("batch acc", bs.accu_mean * 100, "%")
                 #print("progress", (e/epochs) * 100 + batch_iter / len(loader) / epochs * 100, "%")
             batch_iter +=1
-            img = next(loader)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip_limit)
 
     return model, all_batch_stats
 
 def test(model, device, loader):
 
-    batch_size = 64
+    batch_size = 32
 
     all_batch_stats = []
-    for img, label in loader:
+    for img in loader:
+        label = img.label
+        img = img.text
+
+        if img.shape[0] != batch_size:
+            print("Skipping batch with unexpected size")
+            continue
+
+        label = label - 1
+        label = torch.nn.functional.one_hot(label, num_classes=5) 
+        label = label.unsqueeze(1)
         img = img.to(device)
         label = label.to(device)
         with torch.no_grad():
             loss, p = model.forward(img, label)
-        bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == label,
-                                 (torch.argmax(p, dim=1) == label).sum().item()/batch_size,
-                                 p, label, epoch=0)
+        label = label.squeeze(1)
+        bs = BatchStatistics(batch_size, loss, torch.argmax(p, dim=1) == torch.argmax(label, dim=1),
+                                 (torch.argmax(p, dim=1) == torch.argmax(label, dim=1)).sum().item()/batch_size,
+                                 p, torch.argmax(label, dim=1), epoch=0)
         all_batch_stats.append(bs)
 
     return all_batch_stats
@@ -74,8 +91,8 @@ def main():
     if torch.cuda.is_available():
         device = "cuda"
 
-    text = torchtext.data.Field()
-    label = torchtext.data.Field(sequential=False)
+    text = torchtext.data.Field(batch_first=True)
+    label = torchtext.data.Field(sequential=False, batch_first=True)
 
     data_train, data_val, data_test = torchtext.datasets.SST.splits(text, label,
                               fine_grained=True, train_subtrees=False)
@@ -84,18 +101,18 @@ def main():
     label.build_vocab(data_train)
 
     iter_train, iter_val, iter_test = torchtext.data.BucketIterator.splits(
-        (data_train, data_val, data_test), batch_size = 64)
+        (data_train, data_val, data_test), batch_size = 32)
 
     model = SentRNN(embeddings=text.vocab.vectors)
-    model, stats = train(model, device, iter_train, 64, lr=0.01, optimizer="SGD")
+    model, stats = train(model, device, iter_train, 32, lr=0.005, optimizer="Adam")
 
-    analysis = Evaluation(10, 1, stats)
+    analysis = Evaluation(5, 1, stats)
     file_prefix = ""
     analysis.plot_loss(file_prefix + "train_loss.png")
     analysis.plot_accu(file_prefix + "train_accu.png")
     analysis.calc_loss_by_category()
     stats = test(model, device, iter_test)
-    analysis = Evaluation(10, 1, stats)
+    analysis = Evaluation(5, 1, stats)
     analysis.calc_loss_by_category()
     analysis.plot_loss_by_category(file_prefix + "test.png")
     analysis.print_summary()
--- rnn-sentiment/model.py
+++ rnn-sentiment/model.py
@@ -10,17 +10,33 @@ class SentRNN(torch.nn.Module):
         self.out_size = out_size
         
         self.encoder = torch.nn.Embedding(inp_size, h_size)
-        self.recurrent = torch.nn.GRU(h_size, h_size, num_layers=1)
+        self.recurrent = torch.nn.GRU(h_size, h_size, num_layers=1, batch_first=True)
         self.fconnected = torch.nn.Linear(h_size, out_size)
 
         self.encoder.weight.data.copy_(embeddings)
+        self.hidden_state = torch.zeros(1, 32, h_size).to('cuda')
 
     def forward(self, x, label):
-
+        self.hidden_state = torch.zeros(1, 32, 300).to('cuda')
+        #print(x)
         x = self.encoder(x)
-        _, h = self.recurrent(x)
-        x = self.fconnected(h)
-        label = label.double()
-        loss = torch.nn.functional.binary_cross_entropy_with_logits(x, label)
-        return loss.mean(dim=0), x
+        #print("aftger", x)
+        x, _ = self.recurrent(x, self.hidden_state) #self.hidden_state) #, self.hidden_state)
+        #max pool
+        #x, _ = torch.max(x, dim=1)
+        x = x[:, -1, :]
+        #pytorch transposes hidden.
+        #print("x", x.shape)
+        #print("h", self.hidden_state.shape) 
+        x = self.fconnected(x) #torch.transpose(x, 0, 1))
+        #x = x.transpose(0, 1)
+        #label = label.double()
+        x = torch.nn.functional.sigmoid(x)
+        loss = torch.nn.functional.binary_cross_entropy(x, label.squeeze(1).float(), reduction="mean")
+        lp = torch.nn.functional.softmax(x, dim=1)
+
+        #self.hidden_state = self.hidden_state.detach()
+        #self.recurrent.zero_grad()
+        #x is not prob..
+        return loss, x
 
